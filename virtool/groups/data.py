0b07928362e0a1476d60a7e1e0addde4a16c0213
Matthew <matthewcurtis1999@gmail.com>
2023-11-01 10:15:51 -0700

    chore: remove extra imports

--- virtool/groups/data.py
+++ virtool/groups/data.py
@@ -14,7 +14,6 @@ from virtool.groups.mongo import (
 from virtool.groups.oas import UpdateGroupRequest
 from virtool.groups.pg import SQLGroup
 from virtool.mongo.core import Mongo
-from virtool.users.pg import user_group_associations
 from virtool.users.utils import generate_base_permissions
 from virtool.utils import base_processor
 
